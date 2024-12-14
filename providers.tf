87afbd79f5c4c4df1a328171061fa1e65ab15fd1
swansoj2 <swansoj2@uc.edu>
2024-12-13 22:37:05 -0500

    update

--- providers.tf
+++ providers.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.58.0"
+      version = "~> 5.0"
+      configuration_aliases = [aws.shared-services]
     }
   }
 }
